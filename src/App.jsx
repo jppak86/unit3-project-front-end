23c0a716c92796d35b363b349255db7a1737d59e
Jayden Pak <jppak86@gmail.com>
2022-04-02 15:18:28 -0400

    auth setup

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+import * as authService from './services/authService'
+import NavBar from './components/NavBar/NavBar'
+import Signup from './pages/Signup/Signup'
+import Login from './pages/Login/Login'
+import Landing from './pages/Landing/Landing'
+import Profiles from './pages/Profiles/Profiles'
+
+const App = () => {
+  const [user, setUser] = useState(authService.getUser())
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    authService.logout()
+    setUser(null)
+    navigate('/')
+  }
+
+  const handleSignupOrLogin = () => {
+    setUser(authService.getUser())
+  }
+
+  return (
+    <>
+      <NavBar handleLogout={handleLogout} user={user} />
+      <Routes>
+        <Route path="/" element={<Landing user={user} />} />
+        <Route
+          path="/signup"
+          element={<Signup handleSignupOrLogin={handleSignupOrLogin} />}
+        />
+        <Route
+          path="/login"
+          element={<Login handleSignupOrLogin={handleSignupOrLogin} />}
+        />
+        <Route
+          path="/profiles"
+          element={user ? <Profiles /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </>
+  )
+}
+
+export default App
